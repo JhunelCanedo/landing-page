730caec4e4b8c75d1a75c8e53bf8633935f59508
jhunel <jcanedo0628@gmail.com>
2023-06-11 21:42:54 +0800

    test

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["Information.", "Knowledge.", "Awareness.", "Development.", "Enlightenment."],
+    typeSpeed: 100, 
+    backSpeed: 130,
+    loop: true
+  });
